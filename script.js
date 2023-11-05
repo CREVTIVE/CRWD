0efad254ee1b4354770e6b6e47a063e1e70838aa
João Mainardi <JoaoMainardes@users.noreply.github.com>
2023-11-05 12:00:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import zim from "https://zimjs.org/cdn/01/zim";
 // and https://zimjs.com/learn
 // and https://zimjs.com/docs
 
-new Frame(FIT, window.innerWidth, 300, "black", "black", ready, "/assets/people.png");
+new Frame(FIT, window.innerWidth, window.innerHeight, "black", "black", ready, "/assets/people.png");
 function ready() {
    
 	// given F (Frame), S (Stage), W (width), H (height)
@@ -22,7 +22,7 @@ function ready() {
 		const speed = rand(20,40);		
 		new Sprite("/assets/people.png", cols, rows)
 			.run({startFrame:i, endFrame:i})	
-			.pos(-400,-180+i*1.3,LEFT,BOTTOM)
+			.pos(-400, window.innerHeight-180+i*1.3,LEFT,BOTTOM)
 			.animate({
 				props:{y:String(rand(-5,-10))}, // string values are relative amounts
 				time:speed/100,
