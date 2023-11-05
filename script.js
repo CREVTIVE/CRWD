3d588fed3aa1d411c4f5c3bba224fdd66db4252b
João Mainardi <JoaoMainardes@users.noreply.github.com>
2023-11-05 10:22:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,40 +6,40 @@ import zim from "https://zimjs.org/cdn/01/zim";
 
 new Frame(FIT, window.innerWidth, 400, "black", "black", ready, "people.png", "https://assets.codepen.io/1604712/");
 function ready() {
-
-  // given F (Frame), S (Stage), W (width), H (height)
-  // put your code here
-
-  const cols = 15;
-  const rows = 7;
-
-  // make a list of Sprite frames
-  const order = [];
-  loop(cols * rows, i => {order.push(i);});
-
-  // randomize the list and loop 
-  loop(shuffle(order), i => {
-    const speed = rand(20, 40);
-    new Sprite("people.png", cols, rows).
-    run({ startFrame: i, endFrame: i }).
-    pos(-400, -180 + i * 1.3, LEFT, BOTTOM).
-    animate({
-      props: { y: String(rand(-5, -10)) }, // string values are relative amounts
-      time: speed / 100,
-      rewind: true,
-      loop: true,
-      ease: "quadIn" }).
-
-    animate({
-      props: { x: W + 400, flip: true },
-      time: speed,
-      loop: true,
-      rewind: true,
-      ease: "linear" }).
-
-    percentComplete = rand(100);
-  });
-
+   
+	// given F (Frame), S (Stage), W (width), H (height)
+    // put your code here
+	
+	const cols = 15;
+	const rows = 7;
+	
+	// make a list of Sprite frames
+	const order = [];
+	loop(cols*rows, i=>{order.push(i)});	
+	
+	// randomize the list and loop 
+	loop(shuffle(order), i=>{
+		const speed = rand(20,40);		
+		new Sprite("people.png", cols, rows)
+			.run({startFrame:i, endFrame:i})	
+			.pos(-400,-180+i*1.3,LEFT,BOTTOM)
+			.animate({
+				props:{y:String(rand(-5,-10))}, // string values are relative amounts
+				time:speed/100,
+				rewind:true,
+				loop:true,
+				ease:"quadIn"
+			})				
+			.animate({
+				props:{x:W+400, flip:true},
+				time:speed,
+				loop:true,				
+				rewind:true,
+				ease:"linear"
+			})
+			.percentComplete = rand(100);
+	});
+	
 } // end ready
 
 // Docs for items used:
